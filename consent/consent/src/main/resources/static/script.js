7b21737e76e46c2f5bee0ac55367c5fdbb03a028
DJK101 <113543340+DJK101@users.noreply.github.com>
2023-05-23 16:30:49 +0100

    Add style.css and example text

---
+++ consent/consent/src/main/resources/static/script.js
@@ -0,0 +1,10 @@
+const nameDiv = document.getElementById('name');
+const idNumDiv = document.getElementById('idNum');
+
+nameDiv.addEventListener('click', () => {
+    nameDiv.textContent = '';
+});
+
+idNumDiv.addEventListener('click', () => {
+    idNumDiv.textContent = '';
+});
