4848c26e58d7b5debaaa96ec9c43403caea238be
Mark Langtry <m3.langtry@gmail.com>
2023-05-23 17:59:15 +0100

    Working on CSV

--- consent/consent/src/main/java/portfolio/consent/ConsentController.java
+++ consent/consent/src/main/java/portfolio/consent/ConsentController.java
@@ -7,20 +7,20 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 public class ConsentController {
 
-    @PostMapping("/addName")
-    public String consent(@RequestParam("name") String name,@RequestParam("id") int id) {
-        try (FileWriter writer = new FileWriter("portfolio.csv", true)) {
-            writer.append(name);
-            writer.append("\n");
-        } catch (IOException e) {
-            e.printStackTrace();
-            // Handle exception as per your application's requirements
-        }
-
-        return "redirect:/index"; // Redirect to a success page or another URL
+    @PostMapping("/consent")
+    public String consent(@RequestParam("name") String name,@RequestParam("id") int id) throws IOException {
+        String filePath = "portfolio.csv";
+        FileWriter writer = new FileWriter(filePath, true);
+        System.out.println("Hello");
+        String var = name + "," + id;
+        writer.append(var);
+        writer.append("\n");
+        writer.close();
+        return "redirect:/success.html"; // Redirect to a success page or another URL
     }
 }
